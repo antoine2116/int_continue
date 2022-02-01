be4e11939ac9a8393eb9ef8df98384e3bc975c3d
antooinerobin <antooinerobin@gmail.com>
2022-02-01 14:47:17 +0100

    Add test addtionner(), diviser() and soustraire()

--- src/test/java/CalculsTest2.java
+++ src/test/java/CalculsTest2.java
@@ -26,26 +26,84 @@ class CalculsTest2 {
 //		fail("Not yet implemented");
 //	}
 
-    static Stream<Arguments> chargerJeuDeTest() throws Throwable 
+	// region Multiplier
+    static Stream<Arguments> chargerJeuDeTestMultiplier() throws Throwable
     {
         return Stream.of(
-            Arguments.of(2,2,4), // appellera : testMultiplier(2,2,4)
-            Arguments.of(6,6,36),
-            Arguments.of(3,2,6)
+            Arguments.of(2, 2, 4), // appellera : testMultiplier(2,2,4)
+            Arguments.of(6, 6, 36),
+            Arguments.of(3, 2, 3)
         );
     }
 
 	@ParameterizedTest(name="Multiplication numéro {index}: nombre1={0}, nombre2={1}, résultat attendu = {2}")
-	@MethodSource("chargerJeuDeTest")
+	@MethodSource("chargerJeuDeTestMultiplier")
 	void testMultiplier(int firstNumber, int secondNumber, int expectedResult) 
 	{
 		// Partie paramétrée
-	        Calculs monCal = new Calculs(firstNumber, secondNumber);
-	        assertEquals(expectedResult, monCal.multiplier(), "test en échec pour " + firstNumber + " * " + secondNumber + " != " + expectedResult); 
-
-	    // Partie indépendante (les paramètres peuvent ne servir qu'à une sous partie des tests)
-	        String n = null;
-	        assertNull(n);
+		Calculs monCal = new Calculs(firstNumber, secondNumber);
+		assertEquals(expectedResult, monCal.multiplier(), "test en échec pour " + firstNumber + " * " + secondNumber + " != " + expectedResult);
 	}
+
+	// endregion
+
+	// region Additionner
+	static Stream<Arguments> chargerJeuDeTestAdditionner() throws Throwable
+	{
+		return Stream.of(
+				Arguments.of(2, 2, 4), // appellera : testMultiplier(2,2,4)
+				Arguments.of(6, 6, 12),
+				Arguments.of(3, 2, 5)
+		);
+	}
+
+	@ParameterizedTest(name="Additionner numéro {index}: nombre1={0}, nombre2={1}, résultat attendu = {2}")
+	@MethodSource("chargerJeuDeTestAdditionner")
+	void testAdditionner(int firstNumber, int secondNumber, int expectedResult)
+	{
+		// Partie paramétrée
+		Calculs monCal = new Calculs(firstNumber, secondNumber);
+		assertEquals(expectedResult, monCal.additionner(), "test en échec pour " + firstNumber + " + " + secondNumber + " != " + expectedResult);
+	}
+	// endregion
+
+
+	// region Additionner
+	static Stream<Arguments> chargerJeuDeTestDiviser() throws Throwable
+	{
+		return Stream.of(
+				Arguments.of(2, 2, 1),
+				Arguments.of(12, 6, 2)
+		);
+	}
+
+	@ParameterizedTest(name="Diviser numéro {index}: nombre1={0}, nombre2={1}, résultat attendu = {2}")
+	@MethodSource("chargerJeuDeTestDiviser")
+	void testDiviser(int firstNumber, int secondNumber, int expectedResult)
+	{
+		// Partie paramétrée
+		Calculs monCal = new Calculs(firstNumber, secondNumber);
+		assertEquals(expectedResult, monCal.diviser(), "test en échec pour " + firstNumber + " / " + secondNumber + " != " + expectedResult);
+	}
+	// endregion
+
+	// region Additionner
+	static Stream<Arguments> chargerJeuDeTestSoustraire() throws Throwable
+	{
+		return Stream.of(
+				Arguments.of(2, 2, 0),
+				Arguments.of(12, 6, 6)
+		);
+	}
+
+	@ParameterizedTest(name="Soustraire numéro {index}: nombre1={0}, nombre2={1}, résultat attendu = {2}")
+	@MethodSource("chargerJeuDeTestSoustraire")
+	void testSoustraire(int firstNumber, int secondNumber, int expectedResult)
+	{
+		// Partie paramétrée
+		Calculs monCal = new Calculs(firstNumber, secondNumber);
+		assertEquals(expectedResult, monCal.soustraire(), "test en échec pour " + firstNumber + " - " + secondNumber + " != " + expectedResult);
+	}
+	// endregion
 
 }
